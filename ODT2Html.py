b33e9b8f5a2be85157733dae7a3bf67b14e2e977
SManAT <s.hagmann@bgbaden-frauen.ac.at>
2021-09-11 16:55:16 +0200

    work

--- ODT2Html.py
+++ ODT2Html.py
@@ -5,13 +5,9 @@ import os
 
 
 if __name__ == "__main__":
-    """
     app = QApplication(sys.argv)
     screen = app.primaryScreen()
     configure_logging()
     window = MainWindow(screen)
     window.show()
     sys.exit(app.exec())
-    """
-
-    os.system("unoconv -f pdf " + " Ausflugsziele.odt")
